c6dce90bcecb6c7e93ae247d22eb53b74847b1e1
Scott <scott.tolinski@gmail.com>
2017-08-21 10:49:33 -0600

    video #3 on using sass

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby LUT`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`
+  ],
+}
---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ThirdPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to page 3</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default ThirdPage
