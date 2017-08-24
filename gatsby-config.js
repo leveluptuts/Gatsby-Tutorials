6a299c4644cb0e759ff7de3cdeb55182144c4670
Scott <scott.tolinski@gmail.com>
2017-08-24 14:20:22 -0600

    adds build a blog code

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      }
+    },
+    'gatsby-transformer-remark'
   ],
 }
---
+++ src/pages/08-24-2017-first-blog/index.md
@@ -0,0 +1,6 @@
+---
+path: '/first-post'
+title: 'First Blog Post'
+---
+
+Hello! This is our first blog post.
