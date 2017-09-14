af4652249a66a379aa246c59202282a833be599a
Scott <scott.tolinski@gmail.com>
2017-09-14 13:41:32 -0600

    adds blog index

---
+++ src/pages/09-14-2017-second/index.md
@@ -0,0 +1,6 @@
+---
+path: '/second-post'
+title: 'Second Blog Post'
+---
+
+Hi! This is our second blog post.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <h2>Index</h2>
+    {data.allMarkdownRemark.edges.map(post => (
+      <Link 
+        key={post.node.id} 
+        href={post.node.frontmatter.path}>
+        {post.node.frontmatter.title}
+      </Link>
+    ))}
   </div>
 )
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(limit: 10) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
